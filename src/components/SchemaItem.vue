1e81bc18922232cb18b7923f6ec0af6a24d8c2c1
Misha <pakmisha9@gmail.com>
2023-11-17 10:37:52 +0600

    add

--- src/components/SchemaItem.vue
+++ src/components/SchemaItem.vue
@@ -6,14 +6,16 @@ const props = defineProps({
 	}
 })
 
-const imageUrl = new URL(`/src/assets/${props.item.image}.jpg`, import.meta.url)
+function getImageUrl(image) {
+	return new URL(`./src/assets/${image}.jpg`, import.meta.url).href
+}
 </script>
 
 <template>
 	<div class="flex flex-col items-center">
 		<router-link :to="'/schema/' + item.slug" class="relative">
 			<div class="relative aspect-square h-[200px] w-[200px] lg:h-[250px] lg:w-[250px]">
-				<img v-if="item.image != null" :src="imageUrl" class="relative z-30 h-full w-full overflow-hidden rounded-full object-cover" alt="" />
+				<img v-if="item.image != null" :src="getImageUrl(item.image)" class="relative z-30 h-full w-full overflow-hidden rounded-full object-cover" alt="" />
 				<div class="absolute left-1/2 top-1/2 z-20 w-[250px] -translate-x-1/2 -translate-y-1/2 lg:w-[350px]">
 					<img src="@/assets/img/vectors/under-hero.png" class="h-full w-full" alt="" />
 				</div>
